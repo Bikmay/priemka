a20e7cad3b1b01f52c4f722c88cbea687d700165
Alex Frolov <physics_13_2000einstein@mail.ru>
2020-03-27 11:37:44 +0000

    fix

--- app/upinsert.js
+++ app/upinsert.js
@@ -7,11 +7,11 @@ static ConnectInsertAbitur(let datas[]){
 
 			const {Pool, Client}=require('pg');
 			const connectionString='postgressql://postgres:username@passport:localhost/namedatabase';
-			const data='INSERT  INTO entrant(entrant_id,name,bals_of_russian,bals_of_math,bals_of_third,need_campus,type_edu) data($1,$2,$3,$4,$5,$6,$7,$8,$9)';
+			const data='INSERT  INTO entrant(name,bals_of_russian,bals_of_math,bals_of_third,need_campus,type_edu) data($1,$2,$3,$4,$5,$6,$7,$8,$9)';
 			client.query(text,values,(err,res)=>{	
 		if(err){
 			console.log(err.stack)
-			} else{console.log(res.rows[0])	
+			} else{console.log("good")	
 	}
 })
 	pool.end();}
@@ -20,7 +20,7 @@ static ConnectInsertAbitur(let datas[]){
 class TableSpec{
 constructor(){}
 
-static ConnectInsertSpec( let data1[]){
+static ConnectInsertSpec( let datas){
 
 
 
@@ -28,11 +28,11 @@ static ConnectInsertSpec( let data1[]){
 	
 	const {Pool, Client}=require('pg');
 	const connectionString='postgressql://postgres:username@passport:localhost/namedatabase';
-	const data='INSERT  INTO sp_090304(entrant_id, total) data1($1,$2) RETURNING ID ';
+	const data='INSERT  INTO table_name(entrant_id, total) data1($1,$2) RETURNING ID ';
 	client.query(text,values,(err,res)=>{
 			if(err){
 				console.log(err.stack)
-			} else{console.log(res.rows[0])
+			} else{console.log("Good")
 
 	
 				}
